e03b98073d756683a8c91379b0606bd594541bd5
V. Akshaya <akshayavs2013@gmail.com>
2026-02-03 20:34:00 +0530

    Add files via upload

---
+++ Functions/Aggregate Functions/aggregate_functions.sql
@@ -0,0 +1,15 @@
+-- COUNT FUNCTION
+select count(*) from sales;
+select count(dept_id) from employee;
+
+-- SUM FUNCTION
+select sum(amount) from sales;
+
+-- AVG FUNCTION
+select avg(amount) from sales;
+
+-- MIN FUNCTION
+select min(amount) from sales;
+
+-- MAX FUNCTION
+select max(amount) from sales;
